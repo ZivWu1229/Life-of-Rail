bea7ba8e2c0ece734d479019f759ff3c6a777766
ZivWu1229 <zivwu2852@gmail.com>
2022-04-08 00:04:13 +0800

    Create install ursina.bat

---
+++ install ursina.bat
@@ -0,0 +1,7 @@
+pip install ursina
+pip install https://github.com/pokepetter/ursina/archive/master.zip
+pip install git+https://github.com/pokepetter/ursina.git
+git clone https://github.com/pokepetter/ursina.git
+python setup.py develop
+pip install ursina[extras]
+python -m pip install ursina
